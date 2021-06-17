36abd388638a3b120141406bd30beb4cde98b79d
Sebastian Vivas <svivas@correo.unimet.edu.ve>
2021-06-17 19:47:39 -0400

    Statistics Done

--- src/proyecto/pkg2/so/vergel/vivas/Proyecto2SOVergelVivas.java
+++ src/proyecto/pkg2/so/vergel/vivas/Proyecto2SOVergelVivas.java
@@ -1,12 +1,14 @@
 package proyecto.pkg2.so.vergel.vivas;
 
 public class Proyecto2SOVergelVivas {
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       
     }
     
 }
